86d917280ca2ecf842ca2a8824fa7e1f75c2120d
gratom8 <96878370+gratom8@users.noreply.github.com>
2022-02-27 16:20:13 +0200

    rose stuff

--- man_fix.vhd
+++ man_fix.vhd
@@ -7,12 +7,18 @@ use ieee.numeric_std.all;
 
 entity man_fix is 
 	generic(n: integer:= 23);
-	port(inManA,inManB: in std_logic_vector(n-1 downto 0);
-	     outManA,outManB: out std_logic_vector(n+2 downto 0)
+	port(en: in std_logic;                                   --enable signal for unit
+	     inManA,inManB: in std_logic_vector(n-1 downto 0);   --input mantissas of A,B
+	     outManA,outManB: out std_logic_vector(n+2 downto 0) --output mantissas of A,B
 	    );
 end man_fix;
 architecture man_fix_arch of man_fix is
 begin
-	outManA <= '1' & inManA & "00";
-	outManB <= '1' & inManB & "00";
+	process(inManA,inManB)
+	begin
+		if (en='1') then
+			outManA <= '1' & inManA & "00";
+			outManB <= '1' & inManB & "00";
+		end if;
+	end process;
 end man_fix_arch;
